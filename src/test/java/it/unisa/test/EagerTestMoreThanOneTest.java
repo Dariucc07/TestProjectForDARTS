07486d787b899a4635eac36536ba4a69a169460c
gilbertrec <43886382+gilbertrec@users.noreply.github.com>
2021-10-28 12:15:27 +0200

    updating EagerTestMoreThanOne for detection

--- src/test/java/it/unisa/test/EagerTestMoreThanOneTest.java
+++ src/test/java/it/unisa/test/EagerTestMoreThanOneTest.java
@@ -1,28 +1,33 @@
 package it.unisa.test;
 
-import it.unisa.progetto.EagerTestMoreThanOne;
+import it.unisa.progetto.EagerMoreThanOne;
 import org.junit.Test;
 
 public class EagerTestMoreThanOneTest {
-    private EagerTestMoreThanOne moreThanOne;
-    private EagerTestMoreThanOne moreThanOne1;
+    private EagerMoreThanOne moreThanOne;
+    private EagerMoreThanOne moreThanOne1;
 
-    private EagerTestMoreThanOne moreThanOne2;
-    private EagerTestMoreThanOne moreThanOne3;
+    private EagerMoreThanOne moreThanOne2;
+    private EagerMoreThanOne moreThanOne3;
 
     @Test
     /**
      * Method with EAGER TEST
      */
     public void test1(){
-        moreThanOne = new EagerTestMoreThanOne();
-        moreThanOne1 = new EagerTestMoreThanOne();
+        moreThanOne = new EagerMoreThanOne();
+        moreThanOne1 = new EagerMoreThanOne();
 
+        moreThanOne1.a();
+    }
+
+    @Test
+
+    private void refactoredOne() {
         moreThanOne.doSomething(2);
-        moreThanOne.doSomething(33);
+        moreThanOne.doSomething2(33);
 
         moreThanOne1.doSomething(2);
-        moreThanOne1.doSomething(33);
     }
 
     @Test
@@ -30,19 +35,20 @@ public class EagerTestMoreThanOneTest {
      * Method with EAGER TEST
      */
     public void test2(){
-        moreThanOne2 = new EagerTestMoreThanOne();
+        moreThanOne2 = new EagerMoreThanOne();
         moreThanOne2.doSomething(4);
         moreThanOne2.doSomething(44);
 
-        moreThanOne3 = new EagerTestMoreThanOne();
+        moreThanOne3 = new EagerMoreThanOne();
 
         moreThanOne3.doSomething(4);
-        moreThanOne3.doSomething(44);
+        moreThanOne3.doSomething2(44);
+        moreThanOne3.a();
     }
 
     @Test
     public void test3(){
-        moreThanOne = new EagerTestMoreThanOne();
+        moreThanOne = new EagerMoreThanOne();
         moreThanOne.doSomething(2);
     }
 }
